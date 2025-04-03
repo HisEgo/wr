b01c706c068fb28c97fd3701497a396d22857863
mm.abbaszadeh90 <mm.abbaszadeh90@gmail.com>
2025-04-04 02:37:41 +0330

    first commit

---
+++ src/MainFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        setTitle("Super Hexagon");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+    }
+    
+}
