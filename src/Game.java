ba78f7d741572b88f7d2819ccbd835c4e921bd51
mm.abbaszadeh90 <mm.abbaszadeh90@gmail.com>
2025-04-04 02:58:08 +0330

    mainframe edition1

---
+++ src/Game.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Game {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new MainFrame());
+    }
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -9,6 +9,26 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
+
+        JPanel mainMenuPanel = new JPanel();
+        mainMenuPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 50));
+
+        JButton startButton = new JButton("Start");
+        startButton.setPreferredSize(new Dimension(200, 50));
+
+        JButton settingsButton = new JButton("Settings");
+        settingsButton.setPreferredSize(new Dimension(200, 50));
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.setPreferredSize(new Dimension(200, 50));
+
+        mainMenuPanel.add(startButton);
+        mainMenuPanel.add(settingsButton);
+        mainMenuPanel.add(exitButton);
+
+        add(mainMenuPanel, BorderLayout.CENTER);
+
+        setVisible(true);
     }
-    
+
 }
