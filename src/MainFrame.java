dfe118305470a3ef0a5d4fb77a1d8bdd4188a95f
mm.abbaszadeh90 <mm.abbaszadeh90@gmail.com>
2025-04-04 03:30:19 +0330

    add actionlistener to mainframe keys

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
@@ -15,12 +17,32 @@ public class MainFrame extends JFrame {
 
         JButton startButton = new JButton("Start");
         startButton.setPreferredSize(new Dimension(200, 50));
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(MainFrame.this, "Starting the game ...");
+            }
+        });
 
         JButton settingsButton = new JButton("Settings");
         settingsButton.setPreferredSize(new Dimension(200, 50));
 
+        settingsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(MainFrame.this, "Opening settings ...");
+            }
+        });
+
         JButton exitButton = new JButton("Exit");
         exitButton.setPreferredSize(new Dimension(200, 50));
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
 
         mainMenuPanel.add(startButton);
         mainMenuPanel.add(settingsButton);
